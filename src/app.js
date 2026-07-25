007da048f3eb4ae32efff96a717e9809b4770141
Nitin-Jewalikar <nitin.jewalikar@zingworks.co>
2026-07-25 18:03:15 +0530

    updated api routes, added password hashing

--- src/app.js
+++ src/app.js
@@ -1,31 +1,96 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {adminAuth , userAuth} = require("./middlewares/auth")
-const connectDB = require('./config/database')
-const User = require('./model/userModel')
+const { adminAuth, userAuth } = require("./middlewares/auth");
+const connectDB = require("./config/database");
+const User = require("./model/userModel");
+const { signupValidator, updateUserValidator } = require("./utils/validate");
+const bcrypt = require("bcrypt");
 
 connectDB()
   .then(() => {
     console.log("Connected DB successfully");
-    app.listen(7777,()=>{
-    console.log('server listening on port 7777.');
-})
+    app.listen(7777, () => {
+      console.log("server listening on port 7777.");
+    });
   })
   .catch((err) => {
     console.log(err);
   });
-  
 
 app.use(express.json());
 
-app.use('/signup',async(req,res)=>{
-  console.log('sign up', req.body)
-    const user = new User(req.body);
-    try{
-      await user.save();
-      res.send('User saved successfully');
-    }catch(err){
-      console.log(user);
-      res.status(500).send('Internal server error!!')
-    }
-})
+app.post("/signup", async (req, res) => {
+  try {
+    signupValidator(req);
+    const { firstName, lastName, emailID, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailID,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.send("User saved successfully");
+  } catch (err) {
+    res.status(500).send("Internal server error!!" + err);
+  }
+});
+
+// Get user by ID
+app.get("/user/:id", async (req, res) => {
+  try {
+    let user = await User.findById(req.params?.id);
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Internal server error!!");
+  }
+});
+
+//get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne(req.body);
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+// get all users
+app.get("/feed", async (req, res) => {
+  try {
+    let users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Internal server error!");
+  }
+});
+
+//Update user
+app.patch("/user/:userId", async (req, res) => {
+  let id = req.params?.userId;
+  let data = req.body;
+
+  try {
+    updateUserValidator(req);
+    const user = await User.findByIdAndUpdate(id, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully!!");
+  } catch (err) {
+    res.status(500).send("Internal server error!" + err);
+  }
+});
+
+//Delete user
+app.delete("/user", async (req, res) => {
+  try {
+    let user = await User.findByIdAndDelete(req.body.id);
+    res.send("User Deleted successfully!");
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+});
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,14 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    trim: true,
+    required: true,
+    minLength: 2,
+    maxLength: 20
   },
-  lastName: { type: String },
-  emailID: { type: String },
-  password: { type: String },
-  gender: { type: String },
-  age: { type: Number },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  emailID: {
+    type: String,
+    trim: true,
+    unique:true,
+    required: true,
+    maxLength:50,
+    lowercase:true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Invalid emild Id")
+      }
+    }
+
+  },
+  password: {
+    type: String,
+    maxLength:255,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error('Please enter strong password');
+      }
+    }
+  },
+  gender: {
+    type: String,
+    validate(value){
+      console.log('validator runnninggg ', !['male','female','other'].includes(value))
+      if(!['male','female','other'].includes(value)){
+        throw new Error('Gender data is not valid');
+      }
+    }
+  },
+  age: {
+    type: Number,
+    min: 16,
+  },
+  skills: {
+    type: [String],
+  },
+  about: {
+    type: String,
+    default:"Developer"
+  },
+  photoURL: {
+    type: String,
+    trim:true,
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("Not a valid url")
+      }
+    }
+  },
+},{
+  timestamp:true
 });
 
 const User = mongoose.model("User", userSchema);
