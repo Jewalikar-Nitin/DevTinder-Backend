e78d3e6d3fa7d55b6461f9d2c4e99d9c2d9341b0
Nitin-Jewalikar <nitin.jewalikar@zingworks.co>
2026-09-04 18:08:26 +0530

    Added cors middleware to handle cors error

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 connectDB()
@@ -15,6 +16,7 @@ connectDB()
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
